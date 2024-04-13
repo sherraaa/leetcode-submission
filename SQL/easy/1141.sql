2422386ebc76924d2dfff1a5f5dd68416e328f42
sherraaa <102856322+sherraaa@users.noreply.github.com>
2024-04-14 00:16:06 +0700

    Create 1141.sql

---
+++ SQL/easy/1141.sql
@@ -0,0 +1,8 @@
+-- PostgreSQL
+SELECT
+    activity_date AS day,
+    COUNT(DISTINCT user_id) AS active_users
+FROM activity
+WHERE activity_date BETWEEN '2019-06-28' AND '2019-07-27'
+GROUP BY activity_date
+HAVING COUNT(DISTINCT user_id) > 0
